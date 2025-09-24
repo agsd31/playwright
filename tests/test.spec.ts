8a66867e76698666a432a8416fe49bab21f12ce7
Antoine DEFOSSEUX <antoine.defosseux@ecocert.com>
2025-09-24 22:08:12 +0200

    first commit

---
+++ tests/test.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.airbus.com/en/innovation/innovation-ecosystem/airbus-upnext');
+  await page.getByRole('button', { name: 'Accept All Cookies' }).click();
+  await page.locator('#optimate').getByRole('link', { name: 'Learn more' }).click();
+  await page.getByText('Gate-to-gate support').click({
+    button: 'right'
+  });
+  await page.getByText('Gate-to-gate support').click();
+  await expect(page.getByText('Gate-to-gate support')).toBeVisible();
+  await expect(page.locator('#content')).toContainText('VERTEX2');
+});
